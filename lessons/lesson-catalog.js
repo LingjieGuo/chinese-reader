9b1c2c980cdc42b19c2cbd95d021e46f18e1a7a6
LingjieGuo <115125033+LingjieGuo@users.noreply.github.com>
2026-04-12 22:11:10 -0400

    Add files via upload

---
+++ lessons/lesson-catalog.js
@@ -0,0 +1,151 @@
+"use strict";
+
+window.CHINESE_READER_LESSONS = [
+  {
+    id: "rtos-le2-l18-t1",
+    title: "Road to Success, Lower Elementary 2, Lesson 18, Text 1",
+    description: "Lesson 18 Text 1",
+    text: `路人：请问，743路汽车到西单吗？
+丁兰：不到。690路到西单。
+路人：690路车站怎么走？
+丁兰：你从这儿一直往前走，到十字路口往右拐，往西走大概50米。
+路人：西单方向的车站在马路南边还是北边？
+丁兰：南边。你过马路，到路南边等车。
+路人：谢谢！
+丁兰：不谢！`
+  },
+  {
+    id: "rtos-le2-l18-t2",
+    title: "Road to Success, Lower Elementary 2, Lesson 18, Text 2",
+    description: "Lesson 18 Text 2",
+    text: `我们学校附近交通很方便，有公共汽车，还有地铁。
+公共汽车站离学校不远。地铁站离学校大概有500米。
+学校附近没有车到天安门。
+去天安门要先坐690路到西单，然后换37路公共汽车。`
+  },
+  {
+    id: "rtos-le2-l19-t1",
+    title: "Road to Success, Lower Elementary 2, Lesson 19, Text 1",
+    description: "Lesson 19 Text 1",
+    text: `安妮：美爱，你是不是住6号楼？
+李美爱：是啊。
+安妮：听说6号楼的房间不错，是不是？
+李美爱：我觉得挺好的。
+安妮：房间里有电视吗？
+李美爱：有。有电视、电话，还可以上网。
+安妮：房间里可以洗澡吗？
+李美爱：可以。
+安妮：可以做饭吗？
+李美爱：不行。房间里不能做饭。楼里每层有一个公共厨房，可以在那儿做饭。
+安妮：我想换宿舍。你哪天有时间？我可以去你那儿看一下儿吗？
+李美爱：行啊。欢迎！`
+  },
+  {
+    id: "rtos-le2-l19-t2",
+    title: "Road to Success, Lower Elementary 2, Lesson 19, Text 2",
+    description: "Lesson 19 Text 2",
+    text: `我住学校宿舍18号楼，我和同屋两个人住。
+我的房间离教室很近，可以走路来上课。
+房间里只有厕所，没有浴室，不能洗澡，不方便。
+我想换个房间。`
+  },
+  {
+    id: "rtos-le2-l19-t3",
+    title: "Road to Success, Lower Elementary 2, Lesson 19, Text 3",
+    description: "Lesson 19 Text 3",
+    text: `学校里留学生太多了，我不喜欢在学校住。
+我住学校外边，自己一个人住。
+我的房子里有卫生间，有厨房，可以做饭，很方便。
+从我家来学校，可以坐公共汽车，也可以坐地铁。`
+  },
+  {
+    id: "rtos-le2-l20-t1",
+    title: "Road to Success, Lower Elementary 2, Lesson 20, Text 1",
+    description: "Lesson 20 Text 1",
+    text: `马丁：小明在吗？
+方大同：他不在。
+马丁：你知道他在哪儿吗？
+方大同：不知道。找他有事吗？
+马丁：我来还他象棋。
+方大同：你在这儿等一会儿吧。
+马丁：我还有事。请你帮我还他，行吗？
+方大同：行。
+马丁：这是我的手机号码，也请你给他。
+方大同：好。
+马丁：谢谢你！再见！
+方大同：再见！`
+  },
+  {
+    id: "rtos-le2-l20-t2",
+    title: "Road to Success, Lower Elementary 2, Lesson 20, Text 2",
+    description: "Lesson 20 Text 2",
+    text: `小明：
+下午你不在，你朋友马丁来还你象棋。他让我谢谢你。
+他问我你的email，我没有。他叫你给他发个短信，告诉他你的email。
+他的手机号码是13621405678。
+大同`
+  },
+  {
+    id: "rtos-le2-l20-t3",
+    title: "Road to Success, Lower Elementary 2, Lesson 20, Text 3",
+    description: "Lesson 20 Text 3",
+    text: `我的爱好是下棋，我觉得中国的象棋非常有意思。
+我还喜欢书法和中国画儿，想请一位中国老师教我。
+我也爱好运动，现在在学打太极拳。`
+  },
+  {
+    id: "rtos-le2-l21-t1",
+    title: "Road to Success, Lower Elementary 2, Lesson 21, Text 1",
+    description: "Lesson 21 Text 1",
+    text: `大夫：你怎么了？
+山本：大夫，我感冒了，头疼，流鼻涕。
+大夫：咳嗽吗？
+山本：咳嗽。嗓子也很疼。
+大夫：发烧吗？
+山本：我觉得头很热。
+大夫：量一下儿体温吧。
+大夫：37度8。不用打针，我给你开点儿药。
+大夫：回宿舍多休息，多喝水。
+山本：好的。谢谢大夫！`
+  },
+  {
+    id: "rtos-le2-l21-t2",
+    title: "Road to Success, Lower Elementary 2, Lesson 21, Text 2",
+    description: "Lesson 21 Text 2",
+    text: `安妮：山本，听说你病了。你哪儿不舒服？
+山本：我感冒了。早上觉得头很疼，还发烧。
+安妮：现在怎么样？
+山本：现在不发烧了，头也不疼了。
+安妮：明天能去上课吗？
+山本：可以。
+安妮：太好了。你休息吧。明天见。
+山本：好的。谢谢你来看我。`
+  },
+  {
+    id: "rtos-le2-l22-t1",
+    title: "Road to Success, Lower Elementary 2, Lesson 22, Text 1",
+    description: "Lesson 22 Text 1",
+    text: `我叫马丁，1982年出生，今年25岁，属狗。
+我们班山本最大，28岁，大卫最小，20岁。
+我1米8，65公斤。
+我个子很高。大卫更高，他1米85。`
+  },
+  {
+    id: "rtos-le2-l22-t2",
+    title: "Road to Success, Lower Elementary 2, Lesson 22, Text 2",
+    description: "Lesson 22 Text 2",
+    text: `马丁：小明，你弟弟个子真高。有1米85吧？
+小明：没有。他1米83。
+马丁：他多大了？
+小明：今年18岁，明年中学毕业。
+马丁：你奶奶多大年纪了？
+小明：她今年65岁，属蛇。
+马丁：这是谁？
+小明：这是我姐姐的孩子。
+马丁：他几岁了？
+小明：三岁。
+马丁：他挺胖的。有多重？
+小明：18公斤。
+马丁：呵，真重！`
+  }
+];
